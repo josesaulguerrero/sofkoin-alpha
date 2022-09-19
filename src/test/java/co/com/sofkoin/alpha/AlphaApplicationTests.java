558d1818614540fb3668d60d541c49da264bbad4
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-19 13:13:35 -0500

    Set up project skeleton with Spring Initializer

---
+++ src/test/java/co/com/sofkoin/alpha/AlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofkoin.alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
