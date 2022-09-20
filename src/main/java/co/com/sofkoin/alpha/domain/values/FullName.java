8b194582e27069fc85374dcb22b098cd4cd5ba53
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-20 13:57:40 -0500

    Fix: Add Apache's GenericValidator validations
    User aggregate was missing validations on FullName and Password VOs

--- src/main/java/co/com/sofkoin/alpha/domain/values/FullName.java
+++ src/main/java/co/com/sofkoin/alpha/domain/values/FullName.java
@@ -3,47 +3,41 @@ package co.com.sofkoin.alpha.domain.values;
 import co.com.sofka.domain.generic.ValueObject;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
-
-import java.util.regex.Pattern;
+import org.apache.commons.validator.GenericValidator;
 
 @EqualsAndHashCode
 @ToString
 public class FullName implements ValueObject<FullName.Values> {
-  private static final Pattern FULL_NAME_REGEX = Pattern.compile("[a-zA-Z]{3,}");
-  private String name;
-  private String surname;
+    private final String name;
+    private final String surname;
 
-  public FullName(String name, String surname) {
-    if(validateFullName(name, surname)) {
-      this.name = name;
-      this.surname = surname;
+    public FullName(String name, String surname) {
+        String FULL_NAME_REGEX = "[a-zA-Z]{3,}";
+        if (!GenericValidator.matchRegexp(name, FULL_NAME_REGEX) && !GenericValidator.matchRegexp(surname, FULL_NAME_REGEX)) {
+            throw new IllegalArgumentException("Invalid full name (Must contain at least three characters and no numbers).");
+        }
+        this.name = name;
+        this.surname = surname;
     }
-  }
 
-  private static boolean validateFullName(String name, String surname) {
-    if(!FULL_NAME_REGEX.matcher(name).find() && !FULL_NAME_REGEX.matcher(surname).find()) {
-      throw new IllegalArgumentException("Invalid full name (Must contain at least three characters and no numbers).");
+    public interface Values {
+        String name();
+
+        String surname();
     }
-    return true;
-  }
 
-  public interface Values {
-    String name();
-    String surname();
-  }
+    @Override
+    public Values value() {
+        return new Values() {
+            @Override
+            public String name() {
+                return name;
+            }
 
-  @Override
-  public Values value() {
-    return new Values() {
-      @Override
-      public String name() {
-        return name;
-      }
-
-      @Override
-      public String surname() {
-        return surname;
-      }
-    };
-  }
+            @Override
+            public String surname() {
+                return surname;
+            }
+        };
+    }
 }
--- src/main/java/co/com/sofkoin/alpha/domain/values/Password.java
+++ src/main/java/co/com/sofkoin/alpha/domain/values/Password.java
@@ -8,21 +8,14 @@ import org.apache.commons.validator.GenericValidator;
 @EqualsAndHashCode
 @ToString
 public class Password implements ValueObject<String> {
-  private String value;
+  private final String value;
 
   public Password(String value) {
-    if(validatePassword(value)) {
-      this.value = value;
-    }
-  }
-
-  private static boolean validatePassword(String password) {
     String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d#$@!%&*?]{8,30}$";
-    if(!GenericValidator.matchRegexp(password, PASSWORD_REGEX)) {
+    if(!GenericValidator.matchRegexp(value, PASSWORD_REGEX)) {
       throw new IllegalArgumentException("Invalid Password (At least eight characters, one number, one lowercase and one uppercase).");
     }
-
-    return true;
+      this.value = value;
   }
 
   public String value() {
