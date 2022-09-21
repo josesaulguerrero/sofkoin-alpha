a5e9ceb8483df08fbd728b7c9981e86dac583fdd
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-21 18:10:43 -0500

    Finish SignUpUseCase test and fix password regex validation

--- src/main/java/co/com/sofkoin/alpha/domain/user/values/Password.java
+++ src/main/java/co/com/sofkoin/alpha/domain/user/values/Password.java
@@ -11,7 +11,8 @@ public class Password implements ValueObject<String> {
     private final String value;
 
     public Password(String value) {
-        String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d#$@!%&*?]{8,30}$";
+        String PASSWORD_REGEX = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$";
+        System.out.println("value = " + value);
         if (!GenericValidator.isEmail(value) && !GenericValidator.matchRegexp(value, PASSWORD_REGEX)) {
             throw new IllegalArgumentException("Invalid Password (At least eight characters, one number, one lowercase and one uppercase).");
         }
