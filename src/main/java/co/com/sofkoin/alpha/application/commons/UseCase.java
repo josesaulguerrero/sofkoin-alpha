11b61abb09751675affd6e6d548a15838d862dee
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-21 14:42:42 -0500

    Refactor UseCase interface

--- src/main/java/co/com/sofkoin/alpha/application/commons/UseCase.java
+++ src/main/java/co/com/sofkoin/alpha/application/commons/UseCase.java
@@ -3,9 +3,10 @@ package co.com.sofkoin.alpha.application.commons;
 import co.com.sofka.domain.generic.Command;
 import co.com.sofka.domain.generic.DomainEvent;
 import reactor.core.CorePublisher;
+import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
 
-public interface UseCase<T extends Command, K extends CorePublisher<? extends DomainEvent>> extends Function<T, K> {
-    K apply(T command);
+public interface UseCase<T extends Command, K extends DomainEvent> extends Function<Mono<T>, CorePublisher<K>> {
+    CorePublisher<K> apply(Mono<T> command);
 }
