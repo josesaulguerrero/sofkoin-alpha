7d16de862a0a92376aa942146169f92bf6966ce0
DavidDeadly <jdrueda513@gmail.com>
2022-09-25 09:19:41 -0500

    Change token expiration to 5 min

--- src/main/java/co/com/sofkoin/alpha/infrastructure/config/security/jwt/JWTProvider.java
+++ src/main/java/co/com/sofkoin/alpha/infrastructure/config/security/jwt/JWTProvider.java
@@ -23,11 +23,11 @@ import java.util.Map;
 @Data
 @Slf4j
 public class JWTProvider {
-  private static final String SECRETE_KEY = "You°are°few°clicks°away°to°be°fooled";
-  private static final Long VALID_TIME = 3600000L;
+  private static final String SECRET_KEY = "You°are°few°clicks°away°to°be°fooled";
+  private static final Long VALID_TIME = 300000L;
   private static final SecretKey secretKey =
     Keys.hmacShaKeyFor(
-      Base64.getEncoder().encodeToString(SECRETE_KEY.getBytes(StandardCharsets.UTF_8)).getBytes()
+      Base64.getEncoder().encodeToString(SECRET_KEY.getBytes(StandardCharsets.UTF_8)).getBytes()
     );
 
   public String createJwtToken(Authentication authentication) {
