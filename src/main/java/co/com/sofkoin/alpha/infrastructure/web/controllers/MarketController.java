46429c84f03ed880ad2f286143e372ee49ce0986
Katerin Calderon <kccc0713@gmail.com>
2022-09-27 11:34:47 -0500

    change controller method to delete an offer to a patch method that modify the aggregate market

--- src/main/java/co/com/sofkoin/alpha/infrastructure/web/controllers/MarketController.java
+++ src/main/java/co/com/sofkoin/alpha/infrastructure/web/controllers/MarketController.java
@@ -10,7 +10,7 @@ import co.com.sofkoin.alpha.domain.market.commands.PublishP2POffer;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,7 +44,7 @@ public class MarketController {
         );
     }
 
-    @DeleteMapping("delete/offer")
+    @PatchMapping("delete/offer")
     public ResponseEntity<Flux<DomainEvent>> deleteP2POffer(@RequestBody @Valid DeleteP2POffer body) {
         return new ResponseEntity<>(
                 this.deleteP2POfferUseCase.apply(Mono.just(body)),
